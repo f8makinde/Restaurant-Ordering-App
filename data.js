c5ad819228dda895423044d2a86097b38b5b1696
Faith Makinde <faithmakinde0@gmail.com>
2022-10-18 11:52:33 +0100

    renderded all data

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-export const menuArray = [
+  export const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-import { menuArray } from "./datajs";
+import { menuArray } from "/data.js";
+
+function getFeedHtml(menu){
+    let main = "";
+    menu.forEach(display => {
+         console.log(display)
+        main += `
+        <div>
+             <div>
+                 ${display.emoji}
+                 </div>
+                 <div>
+                <h2>
+                   ${display.name}
+                   </h2>
+                   <p>${display.ingredients}</p>
+                   <h3>${display.price}</h3>
+                  </div>
+              <div>
+                    +
+                </div>
+            </div>
+        `
+     
+    })
+   return main;
+   
+}
+ 
+function render(){
+    document.getElementById("content").innerHTML = getFeedHtml(menuArray)
+}
+render();
 
