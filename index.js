c10d116f130cbfd70ae2829d7a9669a762c86fd4
Faith Makinde <faithmakinde0@gmail.com>
2022-10-20 22:42:36 +0100

    adding the modal

--- index.js
+++ index.js
@@ -16,19 +16,52 @@ function getFeedHtml(menu){
                    <p>${display.ingredients}</p>
                    <h3>${display.price}</h3>
                   </div>
-              <div>
+              <div class="plus-btn">
+                   <strong id="plus-btn">
                     +
+                    </strong>
                 </div>
             </div>
+            <hr>
         `
-     
+
+
+\
     })
    return main;
-   
+  
 }
  
 function render(){
     document.getElementById("content").innerHTML = getFeedHtml(menuArray)
+    document.getElementById("content").innerHTML = displayModal(menuArray)
 }
 render();
 
+const plusBtn = document.getElementById("plus-btn");
+ 
+plusBtn.addEventListener("click", displayModal);
+
+function displayModal(){
+  let modal1 = "";
+  modal1 += `
+     <div>
+  <h2>Your order</h2>
+  <div>
+      <h3>${display.name}</h3>
+      <p>remove</p>
+  </div>
+  <div>
+    <h3>${display.price}</h3>
+  </div>
+  <hr>
+  <div>
+  <h2>Total Price</h2>
+  <h3>${display.price}</h3>
+  <button>Complete order</button>
+  </div>
+    </div>
+  `
+  return modal1;
+ 
+}
